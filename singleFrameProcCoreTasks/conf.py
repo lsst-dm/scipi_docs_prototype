8aa60289cfdcd8b4b83afc43a62bff2f787b8143
M.S.S. Gill <astro@mandeep.org>
2017-01-24 18:30:34 -0800

    Make repo consistent with lsst-sphinx-bootstrap-theme.

--- singleFrameProcCoreTasks/conf.py
+++ singleFrameProcCoreTasks/conf.py
@@ -3,15 +3,15 @@
 
 from documenteer.sphinxconfig.stackconf import build_package_configs
 
-import lsst.validate.base
+#import lsst.validate.base
 
 
 _g = globals()
 _g.update(build_package_configs(
     project_name='validate_base',
-    copyright='2016 Association of Universities for '
+    copyright='2017 Association of Universities for '
               'Research in Astronomy, Inc.',
-    version=lsst.validate.base.version.__version__,
+    version=0.1,
     doxygen_xml_dirname=None))
 
 intersphinx_mapping['astropy'] = ('http://docs.astropy.org/en/stable', None)
